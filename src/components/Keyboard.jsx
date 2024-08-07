953c45da527a94c92de893bd82f2ed5948dd1ee3
ElyasafArogeti <elyasaf852@gmail.com>
2024-08-07 12:34:13 +0300

    h

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -8,39 +8,48 @@ const hebrewKeys = [
   ["ק", "ר", "א", "ט", "ו", "ן", "ם", "פ"],
   ["ש", "ד", "ג", "כ", "ע", "י", "ח", "ל", "ך", "ף"],
   ["ז", "ס", "ב", "ה", "נ", "מ", "צ", "ת", "ץ"],
-  ["  ", "שפה"],
+  ["_", "שפה","↩"],
 ];
 
-const Keyboard = ({text ,setText}) => {
-
+const Keyboard = ({text ,setText, arr, setArr }) => {
+  
   const select = (key) => {
-    setText(text + key);
+    if (key === "↩") {
+      const newArr = arr.slice(0, -1);
+      setArr(newArr);
+      setText(newArr.join(""));
+    } else {
+      const newArr = [...arr, key];
+      setArr(newArr);
+      setText(newArr.join(""));
+    }
   };
-
+ 
   return (
     <div className="keyboard">
-      {hebrewKeys.map((row, rowIndex) => (
+      { hebrewKeys.map((row, rowIndex) =>(
         <div className="keyboard-row" key={rowIndex}>
           {row.map((key, keyIndex) => (
-            <div className="key" key={keyIndex} onClick={() => select(key)}>
+            <div className="key" key={keyIndex} onClick={() => select(key)}> 
               {key}
             </div>
           ))}
         </div>
-      ))}
+      )) }
     </div>
   );
 };
 
 const MyKey = () => {
   const [text , setText] = useState("");
-
+  const [arr, setArr] = useState([]);
   return (
     <div className="DisplayView">
       <Display text={text} />
       <h1>מקלדת עברית</h1>
-      <Keyboard  text = {text} setText = {setText}/> 
-     <Size/>        
+      <Keyboard  text = {text} setText = {setText} arr={arr} setArr={setArr}/> 
+      <Size/>
+
     </div>
   );
 };
