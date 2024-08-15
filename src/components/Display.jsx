1122758529f9dc62c24e407a2023f7fc61e4296f
ElyasafArogeti <elyasaf852@gmail.com>
2024-08-15 15:09:20 +0300

    ff

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Display.css';
 
 const Display = (props) => {
+  console.log("ww");
+  
   return (
     <>
       <p className='text' style={{ fontSize: props.fontSize, color: props.color }}>{props.text}</p>
